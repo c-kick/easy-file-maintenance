dee5fb4a24c8f7078f4d9d93877fbf9749e57d4d
Klaas Leussink <hnldesign@hnldesign.nl>
2024-12-05 14:32:06 +0100

    Added example user config

---
+++ config/user-config-example.mjs
@@ -0,0 +1,13 @@
+export default {
+  debugLevel:     "info",
+  scanPath:       "/path/to/scan",
+  recycleBinPath: "/path/to/#recycle",
+  actions:        [
+    "reorganize",
+    "duplicates",
+    "orphans",
+    "permissions",
+    "cleanup",
+    "post-cleanup"
+  ]
+};
