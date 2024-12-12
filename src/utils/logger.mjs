aa0277db6f5ee8632c3d507960315786176e12e9
Klaas Leussink <hnldesign@hnldesign.nl>
2024-12-12 17:00:47 +0100

    Quick timestamp addition to logger. Need to refine this later.

--- src/utils/logger.mjs
+++ src/utils/logger.mjs
@@ -1,4 +1,5 @@
 import ora from 'ora';
+import { performance } from 'perf_hooks'; // Import the performance API
 
 // Combined error handler function
 function globalErrorHandler(error, origin) {
@@ -15,6 +16,7 @@ process.on('unhandledRejection', (error) => globalErrorHandler(error, 'unhandled
 process.on('uncaughtException', (error) => globalErrorHandler(error, 'uncaughtException'));
 
 let spinner = null;
+let startTime = null; // Variable to store the high-resolution start time
 
 const logger = {
   hasInstance: () => {
@@ -23,33 +25,36 @@ const logger = {
   start: (message) => {
     if (!spinner) {
       spinner = ora(message).start();
+      startTime = performance.now(); // Start the high-resolution timer
     } else {
       spinner.text = message;
       spinner.start();
+      startTime = performance.now(); // Reset the timer
     }
     return logger; // Enable chaining
   },
   text: (message) => {
     if (!spinner) {
       spinner = ora().start();
+      startTime = performance.now(); // Start the timer if not already started
     }
     spinner.text = message;
-    //console.log(`[LOG] ${message}`); // Log message while updating spinner
-
     return logger; // Enable chaining
   },
   succeed: (message) => {
     if (spinner) {
-      spinner.succeed(message);
+      const duration = (performance.now() - startTime).toFixed(2); // Calculate the elapsed time in milliseconds
+      spinner.succeed(`${message} (took ${duration} ms)`);
       spinner = null;
     } else {
-      ora().start().succeed(message);
+      ora().start().succeed(`${message}`);
     }
     return logger; // Enable chaining
   },
   warn: (message) => {
     if (!spinner) {
       spinner = ora().start();
+      startTime = performance.now(); // Start the timer if not already started
     }
     spinner.warn(`\x1b[33m${message}\x1b[0m`);
     spinner = null;
@@ -58,6 +63,7 @@ const logger = {
   fail: (message) => {
     if (!spinner) {
       spinner = ora().start();
+      startTime = performance.now(); // Start the timer if not already started
     }
     spinner.fail(`\x1b[31m${message}\x1b[0m`);
     spinner = null;
@@ -75,12 +81,14 @@ const logger = {
     if (spinner) {
       spinner.stop();
       spinner = null;
+      startTime = null; // Clear the timer
     }
     return logger; // Enable chaining
   },
   indent: () => {
     if (!spinner) {
       spinner = ora().start();
+      startTime = performance.now(); // Start the timer if not already started
     }
     spinner.indent = 2;
     return logger; // Enable chaining
